0e0698078c3ea9a6dd071355b180724c9cbf06e1
atchisonbrent <atchisonbrent@gmail.com>
2018-11-02 01:31:37 -0500

    Q2b

--- q2.cu
+++ q2.cu
@@ -19,6 +19,27 @@ __global__ void part_a(int n, int *A, int *B){
     }
 }
 
+/* Part B */
+__global__ void part_b(int n, int *A, int *B){
+    __shared__ int s[10];
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    for (int i = index; i < n; i += stride) {
+        if (0 <= A[i] && A[i] <= 99) { atomicAdd(B, 1); }
+        else if (100 <= A[i] && A[i] <= 199) { atomicAdd(&s[1], 1); }
+        else if (200 <= A[i] && A[i] <= 299) { atomicAdd(&s[2], 1); }
+        else if (300 <= A[i] && A[i] <= 399) { atomicAdd(&s[3], 1); }
+        else if (400 <= A[i] && A[i] <= 499) { atomicAdd(&s[4], 1); }
+        else if (500 <= A[i] && A[i] <= 599) { atomicAdd(&s[5], 1); }
+        else if (600 <= A[i] && A[i] <= 699) { atomicAdd(&s[6], 1); }
+        else if (700 <= A[i] && A[i] <= 799) { atomicAdd(&s[7], 1); }
+        else if (800 <= A[i] && A[i] <= 899) { atomicAdd(&s[8], 1); }
+        else if (900 <= A[i] && A[i] <= 999) { atomicAdd(&s[9], 1); }
+    }
+    __syncthreads();
+    for (int i = 0; i < 10; i++) { atomicAdd(&B[i], 1); }
+}
+
 /* Part C */
 __global__ void part_c(int *B, int *C){
     for (int i = 0; i < 10; i += 1) {
@@ -71,13 +92,28 @@ int main() {
         if (i + 1 != d) { fprintf(f, ", "); }
     } fclose(f);
     
+    /* Part B */
+    int blockSize = 256;
+    int numBlocks = (count + blockSize - 1) / blockSize;
+    part_b<<<numBlocks, blockSize>>>(count, A, B);
+    
+    /* Wait for GPU */
+    cudaDeviceSynchronize();
+    
+    /* Part B to File */
+    FILE *f = fopen("q2b.txt", "w");
+    for (int i = 0; i < d; i++) {
+        fprintf(f, "%d", B[i]);
+        if (i + 1 != d) { fprintf(f, ", "); }
+    } fclose(f);
+    
     /* Print B */
     printf("B: ");
     for (int i = 0; i < d; i++) {
         printf("%d", B[i]);
         if (i + 1 != d ) printf(", ");
     } printf("\n");
-
+    
     /* Copy B to C */
     for (int i = 0; i < d; i++) { C[i] = B[i]; }
     
@@ -94,12 +130,12 @@ int main() {
         if (i + 1 != d) { fprintf(f3, ", "); }
     } fclose(f3);
     
-    /* Print C */
-    printf("C: ");
-    for (int i = 0; i < d; i++) {
-        printf("%d", C[i]);
-        if (i + 1 != d ) printf(", ");
-    } printf("\n");
+//    /* Print C */
+//    printf("C: ");
+//    for (int i = 0; i < d; i++) {
+//        printf("%d", C[i]);
+//        if (i + 1 != d ) printf(", ");
+//    } printf("\n");
     
     /* Free Memory */
     cudaFree(A);
