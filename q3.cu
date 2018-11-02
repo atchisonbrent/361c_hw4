81c7e12554d467a101f294decf3438b3625dc82c
atchisonbrent <atchisonbrent@gmail.com>
2018-11-01 23:42:38 -0500

    Move GPU Access

--- q3.cu
+++ q3.cu
@@ -32,11 +32,6 @@ int main() {
     int *D = new int[M];
     int i, count = 0;
     
-    /* Copy to GPU Memory */
-    printf("Copying to GPU Memory!\n");
-    cudaMallocManaged(&A, M * sizeof(int));
-    cudaMallocManaged(&D, M * sizeof(int));
-    
     /* Read numbers as integers one by one */
     printf("Scanning File!\n");
     while (fscanf(fp, "%d", &i) != EOF) {
@@ -47,6 +42,11 @@ int main() {
     /* Close File */
     printf("Closing File!\n");
     fclose(fp);
+    
+    /* Copy to GPU Memory */
+    printf("Copying to GPU Memory!\n");
+    cudaMallocManaged(&A, M * sizeof(int));
+    cudaMallocManaged(&D, M * sizeof(int));
     
     /* Kernel */
     printf("Accessing GPU!\n");
